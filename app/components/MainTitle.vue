01dd282cb46bef17e319b115344ca6dbedb0baa5
Ismaël Sebbane <ismael.sebbane@gmail.com>
2026-01-05 16:26:24 +0100

    feat: add MainTitle component

---
+++ app/components/MainTitle.vue
@@ -0,0 +1,18 @@
+<script setup>
+onMounted(() => {
+  console.log("MainTitle mounted");
+});
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold text-white">
+    {{ title }}
+  </h1>
+</template>
