f9c95d1ee85f062258160e4f0cf00baa31eaa0b1
Ismaël Sebbane <ismael.sebbane@gmail.com>
2026-01-05 17:54:06 +0100

    refactor: adjust LoadingSection layout , typo size adjustments

--- app/components/sections/LoadingSection.vue
+++ app/components/sections/LoadingSection.vue
@@ -4,12 +4,14 @@ import circleAudiowave from "~/assets/images/circle-audiowave.svg";
 
 <template>
   <div
-    class="h-screen w-screen flex flex-col items-center justify-between fixed inset-0 z-10"
+    class="h-screen w-screen flex flex-col items-center justify-center fixed inset-0 z-10"
   >
     <MainTitle title="Hypersensibles" />
-    <div class="flex flex-col items-center justify-center gap-y-4">
-      <p class="text-white uppercase">Cliquer pour écouter</p>
-      <img :src="circleAudiowave" alt="Circle Audio Wave" class="w-10 h-10" />
+    <div
+      class="flex flex-col place-items-center gap-y-4 absolute left-1/2 bottom-10 -translate-x-1/2"
+    >
+      <p class="text-white uppercase leading-5">Cliquer pour écouter</p>
+      <img :src="circleAudiowave" alt="Circle Audio Wave" class="w-18 h-18" />
     </div>
   </div>
 </template>
